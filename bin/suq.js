4c81bce557dfa7f8fe23b2bea8bc2748596d4d41
Matt <contact@mattmcfarland.com>
2015-08-12 03:20:38 -0400

    add help file

--- bin/suq.js
+++ bin/suq.js
@@ -7,12 +7,34 @@ var argv = require('minimist')(process.argv.slice(2)),
     path = require('path'),
     suq = require('../');
 
-suq(url, function (err, data) {
-    if (!output) {
-        console.log(JSON.stringify(data, null, 2));
-    } else {
-        fs.writeFile(path.join(__dirname, output), JSON.stringify(data, null, 2), function(err){
-            console.log('File ' + output + ' successfully written!');
-        })
-    }
-});
+
+if (argv._[0] === 'help' || argv.h || argv.help
+    || (process.argv.length <= 1 && process.stdin.isTTY)) {
+    return fs.createReadStream(__dirname + '/usage.txt')
+        .pipe(process.stdout)
+        .on('close', function () { process.exit(1) })
+        ;
+}
+
+if (argv.version || argv.v) {
+    return console.log(require('../package.json').version);
+}
+
+
+if (url) {
+    suq(url, function (err, data) {
+        if (!output) {
+            console.log(JSON.stringify(data, null, 2));
+        } else {
+            fs.writeFile(path.join(__dirname, output), JSON.stringify(data, null, 2), function(err){
+                console.log('File ' + output + ' successfully written!');
+            })
+        }
+    });
+} else {
+    return fs.createReadStream(__dirname + '/usage.txt')
+        .pipe(process.stdout)
+        .on('close', function () { process.exit(1) })
+        ;
+}
+
