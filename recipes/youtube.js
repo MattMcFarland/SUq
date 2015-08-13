0f3911bf1ae0c47217f5ead0d30a2890dc8d585e
Rich Lewis <richlewis007@gmail.com>
2015-08-13 14:01:40 -0400

    Added error check and an example YouTube link for example.

--- recipes/youtube.js
+++ recipes/youtube.js
@@ -1,12 +1,13 @@
 var suq = require('suq');
 
-suq('/a/youtube/video', function (err, data) {
+suq('https://www.youtube.com/watch?v=Xft3asYLKo0', function (err, data) {
 
-    var props = data.microdata.items[0].properties;
-    console.log('\n\ntitle:', props.name[0]);
-    console.log('\nthumbnail:', props.thumbnailUrl[0]);
-    console.log('embedURL:', props.embedURL[0]);
-    console.log('\ndescription:', props.description[0]);
-    console.log('\ndatePublished:', props.datePublished[0]);
-
+    if (!err) {      
+        var props = data.microdata.items[0].properties;
+        console.log('\n\ntitle:', props.name[0]);
+        console.log('\nthumbnail:', props.thumbnailUrl[0]);
+        console.log('embedURL:', props.embedURL[0]);
+        console.log('\ndescription:', props.description[0]);
+        console.log('\ndatePublished:', props.datePublished[0]);
+    }
 });
